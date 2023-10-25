d49c949c2ead870197c81b7526ec53c4018527e0
Rune Lauridsen <rune.lauridsen.2@gmail.com>
2023-10-25 11:19:56 +0200

    gradle versions problem?

--- Java/boardgames/shared/src/main/java/boardgames/shared/SharedApplication.java
+++ Java/boardgames/shared/src/main/java/boardgames/shared/SharedApplication.java
@@ -1,4 +1,4 @@
-package com.example.shared;
+package boardgames.shared;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Java/boardgames/shared/src/test/java/boardgames/shared/SharedApplicationTests.java
+++ Java/boardgames/shared/src/test/java/boardgames/shared/SharedApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.shared;
+package boardgames.shared;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
