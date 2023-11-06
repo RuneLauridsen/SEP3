b38241c691ec8ce15b32e9222a3e12bb52470406
juli175h <jbramsen1212@gmail.com>
2023-11-06 10:27:52 +0100

    Login Page

---
+++ CSharp/Boardgames/GameClient/Pages/LogIn.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GameClient.Pages;
+
+public class LogIn_razor : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ CSharp/Boardgames/GameClient/Services/IAuthService.cs
@@ -0,0 +1,13 @@
+﻿namespace GameClient.Data;
+
+public interface IAuthService
+{
+    public Task LoginAsync(string username, string password);
+    public Task LogoutAsync();
+    
+    //Fra aflevering, måske ikke relavant
+    /*public Task<UserCreateResult> RegisterAsync(UserCreate userCreate);
+    public Task<ClaimsPrincipal> GetAuthAsync();
+
+    public Action<ClaimsPrincipal> OnAuthStateChanged { get; set; }*/
+}
