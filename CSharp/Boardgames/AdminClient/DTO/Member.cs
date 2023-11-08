278809dc5557c35aebdd3b7a304b34cbe381ad73
juli175h <jbramsen1212@gmail.com>
2023-11-08 12:16:59 +0100

    UI confirm register

---
+++ CSharp/Boardgames/AdminClient/DTO/Member.cs
@@ -0,0 +1,11 @@
+﻿namespace AdminClient.DTO;
+
+public class Member
+{
+    public int Id { get; set; }
+    public String Username { get; set; }
+    public String FirstName { get; set; }
+    public String LastName { get; set; }
+    public String Email { get; set; }
+    public DateTime RegisterDateTime{ get; set; }
+}
