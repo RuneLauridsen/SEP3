2e794b7ed5c647c96f4fefb89c0b6adc0ccbb3a7
juli175h <jbramsen1212@gmail.com>
2023-11-10 09:55:28 +0100

    AdminServices

---
+++ CSharp/Boardgames/AdminClient/Services/AdminService.cs
@@ -0,0 +1,19 @@
+﻿namespace AdminClient.Services;
+
+public class AdminService : IAdminService
+{
+    public ApproveUserResponse AcceptMember(ApproveUserRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public RejectUserResponse RejectMember(RejectUserRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public GetUsersWaitingForApprovalResponse GetUsersWaitingForApproval(GetUsersWaitingForApprovalRequest request)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ CSharp/Boardgames/AdminClient/Services/IAdminService.cs
@@ -0,0 +1,9 @@
+﻿namespace AdminClient.Services;
+
+public interface IAdminService
+{
+    public ApproveUserResponse AcceptMember(ApproveUserRequest request);
+    public RejectUserResponse RejectMember(RejectUserRequest request);
+
+    public GetUsersWaitingForApprovalResponse GetUsersWaitingForApproval(GetUsersWaitingForApprovalRequest request);
+}
