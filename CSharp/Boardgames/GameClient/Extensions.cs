6fcfafd811388f4e73a566955ac3e4e0895ce15f
Rune Lauridsen <rune.lauridsen.2@gmail.com>
2023-11-24 13:12:33 +0100

    profilbillede + username i navbar

--- CSharp/Boardgames/GameClient/Extensions.cs
+++ CSharp/Boardgames/GameClient/Extensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Components;
 
 namespace GameClient;
 
@@ -6,5 +7,10 @@ public static class Extensions {
     // NOTE(rune): https://stackoverflow.com/a/75747008
     public static void ReloadCurrentPage(this NavigationManager manager) {
         manager.NavigateTo(manager.Uri, true);
+    }
+
+    public static string GetOrDefault(this ClaimsPrincipal claimsPrincipal, string type, string defaultValue) {
+        var claim = claimsPrincipal.Claims.FirstOrDefault(x => x.Type == type);
+        return claim?.Value ?? defaultValue;
     }
 }
