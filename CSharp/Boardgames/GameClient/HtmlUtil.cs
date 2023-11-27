135489fce0e4cb93a12b1f8c0bbee32d8563a2f7
Rune Lauridsen <rune.lauridsen.2@gmail.com>
2023-11-27 14:31:43 +0100

    Vis profilbillede fra db i NavMenu

---
+++ CSharp/Boardgames/GameClient/HtmlUtil.cs
@@ -0,0 +1,11 @@
+﻿namespace GameClient;
+
+public static class HtmlUtil {
+    public static string ToImgSrcString(string? base64, string? contentType) {
+        if (base64 != null && contentType != null) {
+            return string.Format("data:{0};base64, {1}", contentType, base64);
+        } else {
+            return "";
+        }
+    }
+}
