45639f7977a51cc21e90a322d5c3e1ae2bb351e3
Rune Lauridsen <rune.lauridsen.2@gmail.com>
2023-11-29 09:44:33 +0100

    Score sums i tabel

--- CSharp/Boardgames/Shared/DTO/ScoreSum.cs
+++ CSharp/Boardgames/Shared/DTO/ScoreSum.cs
@@ -3,6 +3,7 @@
 public class ScoreSum {
     public required int GameId { get; set; }
     public required int AccountId { get; set; }
+    public required string AccountName { get; set; }
     public required int Score { get; set; }
     public required int Count { get; set; }
 }
