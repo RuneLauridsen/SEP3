ccca3b82619a556fef1183a1558c287ef9e27c90
Rune Lauridsen <rune.lauridsen.2@gmail.com>
2023-11-30 13:29:34 +0100

    account.isAdmin + GetAccountsRequest kun return STATUS_ACCEPTED hvis ikke man er admin

--- SQL/database.sql
+++ SQL/database.sql
@@ -22,7 +22,8 @@ CREATE TABLE account
     hashed_password         varchar NOT NULL,
     profile_picture         varchar NULL, -- NOTE(rune): Som Base 64.
     profile_picture_type    varchar NULL, -- NOTE(rune): MIME content type.
-    created_on              timestamp NOT NULL DEFAULT now()
+    created_on              timestamp NOT NULL DEFAULT now(),
+    is_admin                bool    NOT NULL DEFAULT false
 );
 
 CREATE TABLE game
@@ -59,12 +60,14 @@ CREATE TABLE participant
 --------------------------------
 
 INSERT INTO account
-    (username, first_name, last_name, email, registration_datetime, hashed_password, status)
+    (username, first_name, last_name, email, registration_datetime, hashed_password, status, is_admin)
 VALUES
-    ('BenDover',            'Julie', 'Bramsen', 'julie@juliemail.dk',   now(), 'b025079c90813d4669136b2ed07512204ee05522ba3e647935f1a88daf00fd43', 1),    -- password = 'julie'
-    ('Maja123',             'Maja', 'Brixen', 'maja@majamail.dk',       now(),'f29e94153eb385ba00ebb23aca2deaa24222e449584d1d91af4ff2ccc92c8ba5', 1),    -- password = 'maja'
-    ('Minii',              'Simon', 'Banh', 'simon@simonmail.dk',      now(),'0a5d17d3b19f82f8340d3977609aa9e86b4ad8b9bd71bd9eced9271f1d5b2e4a', 1),    -- password = 'simon'
-    ('rune',                'Rune', 'Lauridsen', 'rune@runemail.dk',   now(),'bab432c8f61b2be04113e49c595cc29a0ed57179a105fa098c270cf0dff3ddee', 1);    -- password = 'runerune'
+    ('BenDover',            'Julie', 'Bramsen', 'julie@juliemail.dk',   now(), 'b025079c90813d4669136b2ed07512204ee05522ba3e647935f1a88daf00fd43',  2, false),    -- password = 'julie'
+    ('Maja123',             'Maja', 'Brixen', 'maja@majamail.dk',       now(),'f29e94153eb385ba00ebb23aca2deaa24222e449584d1d91af4ff2ccc92c8ba5',   2, false),    -- password = 'maja'
+    ('Minii',              'Simon', 'Banh', 'simon@simonmail.dk',      now(),'0a5d17d3b19f82f8340d3977609aa9e86b4ad8b9bd71bd9eced9271f1d5b2e4a',    2, false),    -- password = 'simon'
+    ('rune',                'Rune', 'Lauridsen', 'rune@runemail.dk',   now(),'bab432c8f61b2be04113e49c595cc29a0ed57179a105fa098c270cf0dff3ddee',    2, false),    -- password = 'runerune'
+    ('Bob',                'Bob', 'III', 'bobsep3bob@gmail.com',        now(),'6340a7a742bb4e89ff6e7b106548ba4f9fa29b44d7cd205f5f490ffb01215f7d',   2, true),     -- password = 'bobersej'
+    ('Sundar',             'Sundar', 'Pichai', 'sundar@gmail.com',        now(),'89beafb3790f2fb5917de6baea168ad7d4b9bfa9a4e8961997cb532f561b28d7', 1, true);    -- password = 'sundar'
 
 INSERT INTO game
     (name)
