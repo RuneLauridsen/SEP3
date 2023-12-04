c8e21a521b532add99be6fafb40537518ed2c0f3
Rune Lauridsen <rune.lauridsen.2@gmail.com>
2023-12-04 10:36:27 +0100

    Microsofts veje er uransagelige

---
+++ CSharp/Boardgames/Shared/DummyProgram.cs
@@ -0,0 +1,17 @@
+﻿namespace Shared;
+
+public class DummyProgram {
+    public static void Main() {
+        // NOTE:
+        // Vi har brug for
+        //      <FrameworkReference Include="Microsoft.AspNetCore.App" />
+        // for at bruge ProtectedSessionStorage, men med med
+        //      <FrameworkReference Include="Microsoft.AspNetCore.App" />
+        // kan vi ikke compile uden et entry point.
+        //
+        // IDK hvorfor Microsoft ikke længere distribuerer ProtectedSessionStorage
+        // i en almindelig nuget package...
+
+        throw new Exception("This is not a standalone executable.");
+    }
+}
