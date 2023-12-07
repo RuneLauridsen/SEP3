e2692577c00a65b566e776c46a1de71619188fec
Rune Lauridsen <rune.lauridsen.2@gmail.com>
2023-12-07 12:50:13 +0100

    Test_CheckBeforeAfterDecide

--- CSharp/Boardgames/GameClient.Tests/GlobalUsings.cs
+++ CSharp/Boardgames/GameClient.Tests/GlobalUsings.cs
@@ -1,3 +1,7 @@
 global using Xunit;
 global using static Shared.Data.Messages;
 global using static Shared.Tets.TestData;
+
+// NOTE(rune): Kør ikke test parallelt, da de alle bruger samme SQL database.
+// https://stackoverflow.com/questions/1408175/execute-unit-tests-serially-rather-than-in-parallel
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
