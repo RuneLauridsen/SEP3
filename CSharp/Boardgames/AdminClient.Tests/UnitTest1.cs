e79fc2220f7e2723f6ddd940077d4b421340c025
juli175h <jbramsen1212@gmail.com>
2023-12-07 13:18:19 +0100

    endnu lille fix med admin login test

--- CSharp/Boardgames/AdminClient.Tests/UnitTest1.cs
+++ CSharp/Boardgames/AdminClient.Tests/UnitTest1.cs
@@ -22,31 +22,39 @@ public class UnitTest1 {
     //get users waiting for approvel, alle users skal have status pending
     //get account med account id
     
+    
+    //Tester login funktionen i admin client
     [Fact]
     public async Task Test_AdminLogin() {
         {
+            //Login med admin bruger 
             LoginRequest req = new LoginRequest("Bob", "bobersej", true);
             LoginResponse res = await client.AuthService.LoginAsync(req);
             Assert.True(res.loginSuccessful);
             Assert.Equal("",res.errorReason);
         }
         {
+            //login med admin brugernavn, forkert password
             LoginRequest req = new LoginRequest("Bob", "boberikkesej", true);
             LoginResponse res = await client.AuthService.LoginAsync(req);
             Assert.False(res.loginSuccessful);
             Assert.Equal("Can't find account, invalid credentials",res.errorReason);
         }
+        
         {
+            //Login med ikke admin bruger
+            LoginRequest req = new LoginRequest("rune", "runerune", true);
+            LoginResponse res = await client.AuthService.LoginAsync(req);
+            Assert.False(res.loginSuccessful);
+            Assert.Equal("Admin account required for admin client",res.errorReason);
+        }
+        
+        {
+            //login med ikke registreret account 
             LoginRequest req = new LoginRequest("Marie", "ikkebruger", true);
             LoginResponse res = await client.AuthService.LoginAsync(req);
             Assert.False(res.loginSuccessful);
             Assert.Equal("Can't find account, invalid credentials",res.errorReason);
-        }
-        {
-            LoginRequest req = new LoginRequest("Sundar", "sundar", true);
-            LoginResponse res = await client.AuthService.LoginAsync(req);
-            Assert.False(res.loginSuccessful);
-            Assert.Equal("Account awaiting admin approval",res.errorReason);
         }
         
     }
